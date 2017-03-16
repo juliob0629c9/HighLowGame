eae9c02f0167480eb7dcb223e9a1118c6746ea2e
Julio Ballista <julioballista@yahoo.com>
2017-03-16 04:46:28 +0000

    Complete High low

--- script.js
+++ script.js
@@ -26,12 +26,14 @@ $(document).ready(function(){
         $("#easy").css('visibility', 'hidden');
         $("#medium").css('visibility', 'hidden');
         $("#hard").css('visibility', 'hidden');
+        $("#pvp").css('visibility', 'hidden');
         $("h2").html ('Guess 1-10');
         
         
         $("#submit").click (function(){
     
         var given = $("input").val();
+        parseInt('given', 10);
         
         
         
@@ -77,6 +79,7 @@ $(document).ready(function(){
         $("#easy").css('visibility', 'hidden');
         $("#medium").css('visibility', 'hidden');
         $("#hard").css('visibility', 'hidden');
+        $("#pvp").css('visibility', 'hidden');
         $("h2").html ('Guess 1-50!');
         
         
@@ -84,7 +87,7 @@ $(document).ready(function(){
         $("#submit").click (function(){
     
         var given = $("input").val();
-        
+        parseInt('given', 10);
         
         if (given == answermed){
             easymed.pause();
@@ -127,6 +130,7 @@ $(document).ready(function(){
         $("#easy").css('visibility', 'hidden');
         $("#medium").css('visibility', 'hidden');
         $("#hard").css('visibility', 'hidden');
+        $("#pvp").css('visibility', 'hidden');
         $("h2").html ('Guess 1-100!');
         suspense.play();
         
@@ -134,7 +138,7 @@ $(document).ready(function(){
         $("#submit").click (function(){
     
         var given = $("input").val();
-        
+        parseInt('given', 10);
         
         if (given == answerhard){
             suspense.pause();
@@ -169,6 +173,67 @@ $(document).ready(function(){
         
     });
     
+     $("#pvp").click (function(){
+         $("img").css('visibility', 'visible');
+        $("input").css('visibility', 'visible');
+        $("#submit").css('visibility', 'visible');
+        $("#easy").css('visibility', 'hidden');
+        $("#medium").css('visibility', 'hidden');
+        $("#hard").css('visibility', 'hidden');
+        $("#pvp").css('visibility', 'hidden');
+        $("h2").html ('Guess 1-10');
+        $('h2').css ('visibility', 'hidden');
+        do {
+        var UserInput = prompt ("PICK A NUMBER BETWEEN 0 - 9");
+        }while (UserInput !== "1" && UserInput !== "2" && UserInput !== "3" && UserInput !== "4" && UserInput !== "5" && UserInput !== "6" && UserInput !== "7" && UserInput !== "8" && UserInput !== "9" && UserInput !=="0");
+        do {
+           var ChosenInput = prompt ("How many tries would you like them to have?");
+        }while (ChosenInput !== "1" && ChosenInput !== "2" && ChosenInput !== "3" && ChosenInput !== "4" && ChosenInput !== "5" && ChosenInput !== "6" && ChosenInput !== "7" && ChosenInput !== "8" && ChosenInput !== "9");
+      
+     parseInt('UserInput', 10);
+     easymed.play();
+      $("#submit").click (function(){
+    
+        var given = $("input").val();
+        parseInt('input', 10);
+        
+        if (given === UserInput){
+            easymed.pause();
+            winsong.play();
+         $("p").html ("YOU WIN!");    
+         $("img").attr("src", "https://admin.mashable.com/wp-content/uploads/2013/07/crazy-dance.gif");
+        }
+        else if (given < UserInput) {
+            ChosenInput = ChosenInput-1;
+            $("h3").html (ChosenInput + " tries left" );
+         $("p").html ("GO BIGGER");
+        }
+        else if (given > UserInput) {
+            ChosenInput = ChosenInput-1;
+            $("h3").html (ChosenInput + " tries left" );
+            $("p").html ("GO SMALLER");
+           
+        }   
+        if (ChosenInput === 0) {
+            easymed.pause();
+            losesong.play();
+         $("p").html ("YOU LOSE!!!!");   
+         $("h3").html ("It was " + UserInput );
+         $("#submit").css('visibility', 'hidden');
+         $("#again").css('visibility', 'visible');
+          $("img").attr("src", "https://media.giphy.com/media/a9xhxAxaqOfQs/giphy.gif");
+        }
+     
+     
+    
+     });
+    
+    
+    
+    
+    
    
     
+});
+
 });
